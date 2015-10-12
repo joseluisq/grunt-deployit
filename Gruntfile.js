1ff79018e94cea368e96633145158c25ef23d03b
Jose Luis Quintana <joseluisquintana20@gmail.com>
2015-10-12 00:33:09 -0500

    0.1.0

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+/*
+ * grunt-deployit
+ * https://github.com/quintana-dev/grunt-deployit
+ *
+ * Copyright (c) 2015 José Luis Quintana
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    clean: {
+      tests: ['.tmp']
+    },
+    deployit: {
+      default_options: {
+        options: {
+          tests: true
+        },
+        files: {
+          '.tmp/default_options': [
+            'test/fixtures/server',
+            'test/fixtures/deploy'
+          ]
+        }
+      }
+    },
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+  });
+
+  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  grunt.registerTask('test', ['clean', 'deployit:default_options', 'jshint', 'nodeunit']);
+  grunt.registerTask('default', ['deployit:custom_options']);
+
+};
